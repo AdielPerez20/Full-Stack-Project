00d3f10aeaf5897e7871be12a0d548cdc14e885e
AdielPerez20 <adielperez20@gmail.com>
2018-11-21 18:56:30 +0200

    Create Users

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express'); //to using express framework
 const mongoose = require ('mongoose'); //to using mongoDB
+const bodyParser = require('body-parser');
+
 const users = require('./routes/api/users'); 
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
 
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 // DB config - to get the key from keys.js file
 const db = require('./config/keys').mongoURI;
 
@@ -16,7 +21,7 @@ mongoose
   .catch(err => console.log(err));
 
 //request  & response
-app.get('/', (req,res) => res.send('Hello'));
+app.get('/', (req,res) => res.send('naor'));
 //Use routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
